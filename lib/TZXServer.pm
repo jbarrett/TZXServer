6ef5b9d95e1b5a863080dd71af7ea99d3d3b482e
John Barrett <john@jbrt.org>
2016-05-06 00:52:26 +0100

    Route / template for tape id

--- lib/TZXServer.pm
+++ lib/TZXServer.pm
@@ -9,6 +9,14 @@ get '/' => sub {
     template 'index';
 };
 
+get '/tape/:id' => sub {
+    template 'tape', {
+        tape => rset('Tape')
+            ->hri
+            ->find( route_parameters->get('id') )
+    }
+};
+
 any '/search' => sub {
     my $query = param('q');
     $query = join ' ', @{ $query } if ref $query eq 'ARRAY';
