b5c5c1f307818809f3933198492a094446ed1533
John Barrett <john@jbrt.org>
2016-05-05 22:44:13 +0100

    Searching

--- lib/TZXServer.pm
+++ lib/TZXServer.pm
@@ -9,4 +9,17 @@ get '/' => sub {
     template 'index';
 };
 
+any '/search' => sub {
+    my $query = param('q');
+    $query = join ' ', @{ $query } if ref $query eq 'ARRAY';
+
+    template 'results', {
+        query => $query,
+        tapes => rset('Tape')
+            ->by_title( $query )
+            ->hri
+            ->all_ref,
+    }
+};
+
 1;
