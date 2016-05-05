884d1a3b34aa635034f98b9121c9fe85fe304c6b
John Barrett <john@jbrt.org>
2016-05-05 17:37:32 +0100

    Load Plugin::DBIC

--- lib/TZXServer.pm
+++ lib/TZXServer.pm
@@ -1,10 +1,12 @@
 package TZXServer;
-use Dancer2;
 
-our $VERSION = '0.1';
+use Dancer2;
+use Dancer2::Plugin::DBIC qw/ schema rset /;
+
+our $VERSION = schema('default')->{VERSION};
 
 get '/' => sub {
     template 'index';
 };
 
-true;
+1;
