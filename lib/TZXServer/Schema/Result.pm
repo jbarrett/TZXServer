12c9a57b1f526694ff8a622056c3994ea671a8cf
John Barrett <john@jbrt.org>
2016-05-07 17:10:26 +0100

    Some convenience methods for results

--- lib/TZXServer/Schema/Result.pm
+++ lib/TZXServer/Schema/Result.pm
@@ -4,5 +4,12 @@ use TZXServer::Base::Imports;
 
 extends 'DBIx::Class::Core';
 
+sub schema {
+    $_[0]->result_source->schema;
+}
+
+sub rs {
+    shift->result_source->schema->resultset(@_);
+}
 
 1;
