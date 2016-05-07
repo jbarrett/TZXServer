3ca2b7b057054117782b3a7be59cfabda60e573c
John Barrett <john@jbrt.org>
2016-05-07 17:11:29 +0100

    "Unroll" tapes as they are requested

--- lib/TZXServer.pm
+++ lib/TZXServer.pm
@@ -3,6 +3,9 @@ package TZXServer;
 use Dancer2;
 use Dancer2::Plugin::DBIC qw/ schema rset /;
 
+use Try::Tiny;
+use URI;
+
 our $VERSION = schema('default')->{VERSION};
 
 get '/' => sub {
@@ -10,10 +13,24 @@ get '/' => sub {
 };
 
 get '/tape/:id' => sub {
+    my $tape = rset('Tape')
+        ->find( route_parameters->get('id') );
+
+    if ( !$tape ) {
+        status 404;
+        return "Not found"
+    }
+
+    try {
+        $tape->unroll;
+    }
+    catch {
+        status 500;
+        return $_;
+    };
+
     template 'tape', {
-        tape => rset('Tape')
-            ->hri
-            ->find( route_parameters->get('id') )
+        tape => $tape,
     }
 };
 
